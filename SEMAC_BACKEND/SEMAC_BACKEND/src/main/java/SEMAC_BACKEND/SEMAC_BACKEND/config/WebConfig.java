64f20d7aa3f26a19096b7b6f5948d39073f2c7fe
ryan <ryan.c.aguiiarr@gmail.com>
2025-09-26 20:05:49 -0300

    ajuste de cors

--- SEMAC_BACKEND/SEMAC_BACKEND/src/main/java/SEMAC_BACKEND/SEMAC_BACKEND/config/WebConfig.java
+++ SEMAC_BACKEND/SEMAC_BACKEND/src/main/java/SEMAC_BACKEND/SEMAC_BACKEND/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Aplica a política de CORS a todos os endpoints ("/**")
-                .allowedOrigins("http://localhost:5173") // Permite requisições desta origem
+                .allowedOrigins("https://semac-gm3a3xojo-ryans-projects-ff14b62a.vercel.app/**")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Libera os métodos HTTP
                 .allowedHeaders("*") // Libera todos os cabeçalhos
                 .allowCredentials(true);
